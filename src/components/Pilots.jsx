3bfe33a82a350612efcb55138cfc56eec14aa8ba
Abhishek Bhatia <abhishekbhatia@lin.lan>
2022-03-12 20:19:30 -0500

    pilot component setup

---
+++ src/components/Pilots.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { getPilots } from "../../services/sw-api";
+
+class PilotList extends Component {
+  state = {
+    pilots: [],
+  };
+
+  async componentDidMount() {
+    let pilots = await getPilots(this.props.pilots);
+    this.setState({ pilots: pilots });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.pilots.map((pilot, index) => {
+          return <li>{pilot.name}</li>;
+        })}
+      </ul>
+    );
+  }
+}
+
+export default PilotList;
